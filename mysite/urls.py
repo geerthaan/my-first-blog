00730500b8d40a5c40d0a446274415f09c5c843f
Geert Haan <g.haan@smb20029.fritz.box>
2018-09-08 10:32:52 +0200

    added path import

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include
 from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
